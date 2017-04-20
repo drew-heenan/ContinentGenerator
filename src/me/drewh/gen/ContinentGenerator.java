b47030163ba32d7cf6e7f56750cffcc718be9c2e
Drew <drewheenan27@gmail.com>
2017-04-20 18:09:51 -0400

    Added context ('from' variable) as the FillPredicate

--- src/me/drewh/gen/ContinentGenerator.java
+++ src/me/drewh/gen/ContinentGenerator.java
@@ -23,11 +23,11 @@ public class ContinentGenerator {
     
     public static void main(String[] args) {
         ContinentGenerator gen = new ContinentGenerator();
-        gen.setSize(2000);
-        gen.setXScale(0.02);
-        gen.setYScale(0.02);
-        gen.setMinimumLakeSize(600);
-        gen.setExpansionChanceMultiplier(4.0);
+        gen.setSize(1024);
+        gen.setXScale(0.04);
+        gen.setYScale(0.04);
+        gen.setMinimumLakeSize(0);
+        gen.setExpansionChanceMultiplier(6);
         gen.generate();
     }
     
@@ -78,7 +78,7 @@ public class ContinentGenerator {
         BufferedImage img = new BufferedImage(this.size, this.size, BufferedImage.TYPE_INT_RGB);
         for(int x = 0; x < this.size; x++)
             for(int y = 0; y < this.size; y++)
-                img.setRGB(x, y, new Color((this.coastalTiles.contains(new Point(x, y)) || this.lakeShoreTiles.contains(new Point(x, y))) ? 255 : 0, (int)(255*heightMap[x][y]), 0).getRGB());
+                img.setRGB(x, y, new Color(this.landMap[x][y] ? 100 : 0, (int)(255*heightMap[x][y]), 0).getRGB());
         try {
             ImageIO.write(img, "png", new File("out.png"));
         } catch (IOException ex) {
@@ -156,7 +156,7 @@ public class ContinentGenerator {
             for(int y = 0; y < this.size; y++) {
                 if(!this.seaMap[x][y] && !this.landMap[x][y]) {
                     List<Point> checked = new ArrayList<>();
-                    fill(new Point(x, y), pt -> {
+                    fill(new Point(x, y), (from, pt) -> {
                         if(!this.seaMap[pt.x][pt.y] && !this.landMap[pt.x][pt.y] && !checked.contains(pt)) {
                             checked.add(pt);
                             return checked.size() < this.minimumLakeSize;
@@ -174,7 +174,7 @@ public class ContinentGenerator {
 	private boolean[][] fillSea() {
 		//Fill in the sea.
         boolean[][] seaMap = new boolean[this.size][this.size];
-        Predicate<Point> seaFillPredicate = pt -> {
+        FillPredicate seaFillPredicate = (from, pt) -> {
             if(pt.x >= 0 && pt.x < this.size && pt.y >= 0 && pt.y < this.size)
                 if(!seaMap[pt.x][pt.y] && !this.landMap[pt.x][pt.y]) {
                     seaMap[pt.x][pt.y] = true;
@@ -202,9 +202,7 @@ public class ContinentGenerator {
                 voronoiMap[x][y] = voronoiNoise.getValue(this.xScale*x, this.yScale*y, 0);
             }
         }
-        
-        Random rand = new Random(this.seed);
-        
+                
         //Generate a landmass.
         boolean[][] landMap = new boolean[this.size][this.size];
         Point fromPt = new Point(this.size/2, this.size/2);//new Point(this.size/2 + (int)(radius*Math.cos(theta)), this.size/2 + (int)(radius*Math.sin(theta)));
@@ -213,13 +211,13 @@ public class ContinentGenerator {
         while(!cellFillStack.isEmpty()) {
             Point cellPt = cellFillStack.pop();
             double fillValue = voronoiMap[cellPt.x][cellPt.y];
-            fill(cellPt, pt -> {
+            fill(cellPt, (from, pt) -> {
                 if(pt.x >= 0 && pt.x < this.size && pt.y >= 0 && pt.y < this.size)
                     if (!landMap[pt.x][pt.y]) {
                         if(voronoiMap[pt.x][pt.y] == fillValue) {
                             landMap[pt.x][pt.y] = true;
                             return true;
-                        } else if(rand.nextDouble() < this.expansionChanceMultiplier / pt.distance(fromPt)) {
+                        } else if(this.rand.nextDouble() < this.expansionChanceMultiplier / pt.distance(fromPt)) {
                             cellFillStack.push(pt);
                         }
                     }
@@ -229,21 +227,27 @@ public class ContinentGenerator {
 		return landMap;
 	}
     
-    public static void fill(Point startPoint, Predicate<Point> fillPredicate) {
-        Stack<Point> fillStack = new Stack<>();
-        fillStack.push(startPoint);
+    public static void fill(Point startPoint, FillPredicate fillPredicate) {
+        Stack<Point[]> fillStack = new Stack<>();
+        fillStack.push(new Point[]{startPoint, startPoint});
         while(!fillStack.isEmpty()) {
-            Point pt = fillStack.pop();
-            if(!fillPredicate.test(pt))
+            Point[] pts = fillStack.pop();
+            if(!fillPredicate.shouldFill(pts[0], pts[1]))
                 continue;
             for(int dx = -1; dx <= 1; dx++) {
                 for(int dy = -1; dy <= 1; dy++) {
-                    if(dx != dy) {
-                        fillStack.push(new Point(pt.x + dx, pt.y + dy));
+                    if(!(dx == 0 && dy == 0)) {
+                        fillStack.push(new Point[]{pts[1], new Point(pts[1].x + dx, pts[1].y + dy)});
                     }
                 }
             }
         }
+    }
+    
+    public static interface FillPredicate {
+    	
+    	public boolean shouldFill(Point from, Point pt);
+    	
     }
 
     
