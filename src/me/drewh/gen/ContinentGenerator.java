a3114d8b051ddf9fef5eb93c3d41dfa09361b263
Drew <drewheenan27@gmail.com>
2017-04-20 17:21:38 -0400

    Fixed file system problem

--- src/me/drewh/gen/ContinentGenerator.java
+++ src/me/drewh/gen/ContinentGenerator.java
@@ -23,8 +23,10 @@ public class ContinentGenerator {
     
     public static void main(String[] args) {
         ContinentGenerator gen = new ContinentGenerator();
-        gen.setXScale(0.05);
-        gen.setYScale(0.06);
+        gen.setSize(2000);
+        gen.setXScale(0.02);
+        gen.setYScale(0.02);
+        gen.setMinimumLakeSize(600);
         gen.setExpansionChanceMultiplier(4.0);
         gen.generate();
     }
@@ -32,7 +34,13 @@ public class ContinentGenerator {
     private int size = 1000, seed = 0;
     private int minimumLakeSize = 100;
     private double xScale = 0.01, yScale = 0.01;
-    private double expansionChanceMultiplier = 3.0;
+    private double expansionChanceMultiplier = 1.0;
+    
+    private boolean[][] landMap, seaMap;
+    private double[][] heightMap;
+    private Random rand;
+    private List<Point> coastalTiles, lakeShoreTiles;
+    
     
     public void setSeed(int seed) {
         this.seed = seed;
@@ -59,7 +67,134 @@ public class ContinentGenerator {
     }
     
     public void generate() {
-        double[][] voronoiMap = new double[this.size][this.size];
+    	this.rand = new Random(this.seed);
+        this.landMap = createLandmass();
+        this.seaMap = fillSea();
+        removeSmallLakes();
+        findShoreTiles();
+        this.heightMap = calculateGeneralAltitude();
+        
+        //Save the output in the landMap, for now
+        BufferedImage img = new BufferedImage(this.size, this.size, BufferedImage.TYPE_INT_RGB);
+        for(int x = 0; x < this.size; x++)
+            for(int y = 0; y < this.size; y++)
+                img.setRGB(x, y, new Color((this.coastalTiles.contains(new Point(x, y)) || this.lakeShoreTiles.contains(new Point(x, y))) ? 255 : 0, (int)(255*heightMap[x][y]), 0).getRGB());
+        try {
+            ImageIO.write(img, "png", new File("out.png"));
+        } catch (IOException ex) {
+            Logger.getLogger(ContinentGenerator.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+	private double[][] calculateGeneralAltitude() {
+		//Calculate general altitude
+        double[][] heightMap = new double[this.size][this.size];
+        double greatestGeneralAltitude = 0, lowestGeneralAltitude = Double.MAX_VALUE;
+        for(int x = 0; x < this.size; x++) {
+        	for(int y = 0; y < this.size; y++) {
+        		if(!this.seaMap[x][y]) {
+        			double nearestLakeShore = Double.MAX_VALUE, nearestCoast = Double.MAX_VALUE;
+        			for(Point coast : this.coastalTiles) {
+        				double dist2 = coast.distanceSq(x, y);
+        				if(dist2 < nearestCoast)
+        					nearestCoast = dist2;
+        			}
+        			for(Point shore : this.lakeShoreTiles) {
+        				double dist2 = shore.distanceSq(x, y);
+        				if(dist2 < nearestLakeShore)
+        					nearestLakeShore = dist2;
+        			}
+        			if(this.lakeShoreTiles.isEmpty() || nearestLakeShore == 0)
+        				nearestLakeShore = -0.1;
+        			nearestLakeShore = Math.pow(nearestLakeShore, 0.1);
+        			if(!this.landMap[x][y])
+        				nearestLakeShore = -10*nearestLakeShore;
+        			double rawAltitude = nearestLakeShore + nearestCoast;
+        			if(rawAltitude > greatestGeneralAltitude)
+        				greatestGeneralAltitude = rawAltitude;
+        			if(rawAltitude < lowestGeneralAltitude)
+        				lowestGeneralAltitude = rawAltitude;
+        			heightMap[x][y] = rawAltitude;
+        		}
+        	}
+        }
+        for(int x = 0; x < this.size; x++) {
+        	for(int y = 0; y < this.size; y++) {
+        		if(!this.seaMap[x][y])
+        			heightMap[x][y] = (heightMap[x][y] - lowestGeneralAltitude) / (greatestGeneralAltitude - lowestGeneralAltitude);
+        	}
+        }
+		return heightMap;
+	}
+
+	private void findShoreTiles() {
+		//Find coastal and lakeshore tiles
+		this.coastalTiles = new ArrayList<>();
+        this.lakeShoreTiles = new ArrayList<>();
+        for(int x = 0; x < this.size; x++) {
+            CONT: for(int y = 0; y < this.size; y++) {
+                if(!this.landMap[x][y])
+                    for(int dx = -1; dx <= 1; dx++) 
+                        for(int dy = -1; dy <= 1; dy++) {
+                            int rx = x + dx, ry = y + dy;
+                            if(rx >= 0 && rx < this.size && ry >= 0 && ry < this.size)
+                                if(dx != dy && this.landMap[rx][ry]) {
+                                	if(this.seaMap[x][y])
+                                		this.coastalTiles.add(new Point(x, y));
+                                	else
+                                		this.lakeShoreTiles.add(new Point(x, y));
+                                    continue CONT;
+                                }
+                        }
+            }
+        }
+	}
+
+	private void removeSmallLakes() {
+		//Fill in lakes that are too small.
+        for(int x = 0; x < this.size; x++) {
+            for(int y = 0; y < this.size; y++) {
+                if(!this.seaMap[x][y] && !this.landMap[x][y]) {
+                    List<Point> checked = new ArrayList<>();
+                    fill(new Point(x, y), pt -> {
+                        if(!this.seaMap[pt.x][pt.y] && !this.landMap[pt.x][pt.y] && !checked.contains(pt)) {
+                            checked.add(pt);
+                            return checked.size() < this.minimumLakeSize;
+                        }
+                        return false;
+                    });
+                    if(checked.size() < this.minimumLakeSize) 
+                        for(Point pt : checked)
+                            this.landMap[pt.x][pt.y] = true;
+                }
+            }
+        }
+	}
+
+	private boolean[][] fillSea() {
+		//Fill in the sea.
+        boolean[][] seaMap = new boolean[this.size][this.size];
+        Predicate<Point> seaFillPredicate = pt -> {
+            if(pt.x >= 0 && pt.x < this.size && pt.y >= 0 && pt.y < this.size)
+                if(!seaMap[pt.x][pt.y] && !this.landMap[pt.x][pt.y]) {
+                    seaMap[pt.x][pt.y] = true;
+                    return true;
+                }
+            return false;
+        };
+        for(int x = 0; x < this.size; x++) {
+            fill(new Point(x, 0), seaFillPredicate);
+            fill(new Point(x, this.size - 1), seaFillPredicate);
+        }
+        for(int y = 0; y < this.size; y++) {
+            fill(new Point(0, y), seaFillPredicate);
+            fill(new Point(this.size - 1, y), seaFillPredicate);
+        }
+		return seaMap;
+	}
+
+	private boolean[][] createLandmass() {
+		double[][] voronoiMap = new double[this.size][this.size];
         Voronoi voronoiNoise = new Voronoi();
         voronoiNoise.setSeed(this.seed);
         for(int x = 0; x < this.size; x++) {
@@ -91,73 +226,8 @@ public class ContinentGenerator {
                 return false;
             });
         }
-        
-        //Fill in the sea.
-        boolean[][] seaMap = new boolean[this.size][this.size];
-        Predicate<Point> seaFillPredicate = pt -> {
-            if(pt.x >= 0 && pt.x < this.size && pt.y >= 0 && pt.y < this.size)
-                if(!seaMap[pt.x][pt.y] && !landMap[pt.x][pt.y]) {
-                    seaMap[pt.x][pt.y] = true;
-                    return true;
-                }
-            return false;
-        };
-        for(int x = 0; x < this.size; x++) {
-            fill(new Point(x, 0), seaFillPredicate);
-            fill(new Point(x, this.size - 1), seaFillPredicate);
-        }
-        for(int y = 0; y < this.size; y++) {
-            fill(new Point(0, y), seaFillPredicate);
-            fill(new Point(this.size - 1, y), seaFillPredicate);
-        }
-        
-        //Fill in lakes that are too small.
-        for(int x = 0; x < this.size; x++) {
-            for(int y = 0; y < this.size; y++) {
-                if(!seaMap[x][y] && !landMap[x][y]) {
-                    List<Point> checked = new ArrayList<>();
-                    fill(new Point(x, y), pt -> {
-                        if(!seaMap[pt.x][pt.y] && !landMap[pt.x][pt.y] && !checked.contains(pt)) {
-                            checked.add(pt);
-                            return checked.size() < this.minimumLakeSize;
-                        }
-                        return false;
-                    });
-                    if(checked.size() < this.minimumLakeSize) 
-                        for(Point pt : checked)
-                            landMap[pt.x][pt.y] = true;
-                }
-            }
-        }
-        
-        //Find coastal and lakeshore tiles
-        List<Point> coastalTiles = new ArrayList<>();
-        for(int x = 0; x < this.size; x++) {
-            CONT: for(int y = 0; y < this.size; y++) {
-                if(seaMap[x][y])
-                    for(int dx = -1; dx <= 1; dx++) 
-                        for(int dy = -1; dy <= 1; dy++) {
-                            int rx = x + dx, ry = y + dy;
-                            if(rx >= 0 && rx < this.size && ry >= 0 && ry < this.size)
-                                if(dx != dy && landMap[rx][ry]) { 
-                                    coastalTiles.add(new Point(x, y));
-                                    continue CONT;
-                                }
-                        }
-            }
-        }
-        
-        //Save the output in the landMap, for now
-        BufferedImage img = new BufferedImage(this.size, this.size, BufferedImage.TYPE_INT_RGB);
-        for(int x = 0; x < this.size; x++)
-            for(int y = 0; y < this.size; y++)
-                img.setRGB(x, y, new Color(coastalTiles.contains(new Point(x, y)) ? 255 : 0, landMap[x][y] ? 255 : 0, seaMap[x][y] ? 255 : 0).getRGB());
-        try {
-            ImageIO.write(img, "png", new File("out.png"));
-        } catch (IOException ex) {
-            Logger.getLogger(ContinentGenerator.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
+		return landMap;
+	}
     
     public static void fill(Point startPoint, Predicate<Point> fillPredicate) {
         Stack<Point> fillStack = new Stack<>();
